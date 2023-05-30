c090008a107c5bcfa9a565b20ea35c200ae70587
NaZe0320 <rwt1226@naver.com>
2023-05-30 23:53:58 +0900

    네이버 로그인 Callback 테스트

--- LoginTest/app/src/main/java/com/naze/logintest/MainActivity.kt
+++ LoginTest/app/src/main/java/com/naze/logintest/MainActivity.kt
@@ -2,7 +2,10 @@ package com.naze.logintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.OAuthLoginCallback
 import com.naze.logintest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        NaverIdLoginSDK.initialize(context = this, "R5gEhRiOXchm2GYkJLE0","1QWa1OlsSI","LoginTest")
+        //네아로 객체 초기화
+        setNaverLogin()
+
+    }
+
+    private fun setNaverLogin() {
+        val oauthLoginCallback = object : OAuthLoginCallback {
+            override fun onError(errorCode: Int, message: String) {
+                Log.d("Login Test Error","$errorCode : $message")
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                Log.d("Login Test Failure","${NaverIdLoginSDK.getLastErrorCode()} : $message")
+            }
+
+            override fun onSuccess() {
+                Log.d("Login Test Success","Success ${NaverIdLoginSDK.getAccessToken()}")
+            }
+        }
+
+        binding.btnOAuthLoginImg.setOAuthLogin(oauthLoginCallback)
     }
 }
