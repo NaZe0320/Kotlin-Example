49a0baa533ca319bcc2413c2996b901592dcaae5
NaZe0320 <rwt1226@naver.com>
2023-06-05 21:12:54 +0900

    이미지 슬라이더 이전 페이지 표시 시도 중

--- ImageSlideTest/app/src/main/java/com/naze/imageslidetest/MainActivity.kt
+++ ImageSlideTest/app/src/main/java/com/naze/imageslidetest/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.naze.imageslidetest
 
 import android.annotation.SuppressLint
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.internal.DescendantOffsetUtils
@@ -42,10 +45,20 @@ class MainActivity : AppCompatActivity() {
             adapter = imageAdapter
         }
 
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin)
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pagerWidth)
+        val screenWidth = resources.displayMetrics.widthPixels
+        val offsetPx = screenWidth - pageMarginPx - pagerWidth
+
+        binding.viewPager.setPageTransformer { page, position ->
+            page.translationX = position * -offsetPx
+        }
+
         val nextItem = Int.MAX_VALUE / 2 - ceil(images.size.toDouble() / 2 ).toInt() //무한 스크롤링
         binding.viewPager.setCurrentItem(nextItem, false)
         binding.tvViewCount.text = "${nextItem%images.size+1} / ${images.size}"
         binding.tvInfo.text = texts[nextItem%images.size]
+
 
         binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
@@ -55,10 +68,6 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-/*        binding.viewPager.setPageTransformer { page, position ->
-            val offset = resources.getDimensionPixelOffset(R.dimen.viewpager_next_item_offset)
-            page.translationX = -position * offset
-        }*/
 
 
     }
